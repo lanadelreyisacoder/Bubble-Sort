fc0b2922657a740ccd32480b194186946deef786
Oscar Ahuactzin <78440494+lanadelreyisacoder@users.noreply.github.com>
2025-10-07 16:15:38 -0600

    Add files via upload

---
+++ BubbleSort.vb
@@ -0,0 +1,42 @@
+Option Explicit Off 'Para no tener que declarar i, j, k, etc dentro de ciclos for.
+
+Module VBModule
+    Sub Main()
+        Dim numeros As Double() = { 8, 4, 1, 2, 9, 7, 3 }
+        
+        Dim bs As New BubbleSort()
+        bs.Sort(numeros)
+        
+    End Sub
+End Module
+
+Module AnotherModule
+    Public Class BubbleSort
+        Public Sub Sort(ByRef myArray() As Double)
+            
+            Dim temp As Double
+            
+            For i = LBound(myArray) + 1 To UBound(myArray) Step 1
+                
+                For j = LBound(myArray) To UBound(myArray) - i Step 1
+                    
+                    If myArray(j) > myArray(j + 1) Then
+                        temp = myArray(j)
+                        myArray(j) = myArray(j + 1)
+                        myArray(j + 1) = temp
+                    End If
+                    
+                Next j
+            
+            Next i
+            
+            'Impimir colección.
+            For k = LBound(myArray) To UBound(myArray) Step 1
+                
+                Console.Write(myArray(k) & " ")
+                
+            Next k
+                
+        End Sub
+    End Class
+End Module
---
+++ Readme.txt
@@ -0,0 +1,19 @@
+54032
+
+45032'
+40532'
+40352'
+40325'
+
+0432'5
+0342'5
+0324'5
+
+032'45
+023'45
+
+02'345
+
+En este ejemplo concreto, para j, su límite será j + 1, ya que, para cada pase se le resta i al último índice que es 4 y que comienza en 1 y para hasta alcanzar los pases que equivalen al total de numeros en la colección - 1.
+
+Si no se le resta esa i, ocurrirá un IndexOutOfRange.
